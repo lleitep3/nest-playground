b9d4993510d44596e45e832dab3f3570c3b01a7d
Leandro Leite <leandro@leandroleite.info>
2022-07-10 11:50:57 -0300

    feat: app structure

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot()
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
